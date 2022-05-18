884228e9248c895461074de90a1f59bfa1ca8455
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-05-17 22:32:39 -0500

    Minor changes

--- ask_usr.py
+++ ask_usr.py
@@ -1,3 +1,9 @@
+def check_num(num):
+    '''Function to check no weird values are given by the user'''
+    # Check that numbers with no more than 2 decimals are given
+    # Check that no negative numbers are given
+    pass
+
 def ask_usr_num(option):
     num = '1'
     while type(num) != type(1) and type(num) != type(1.0):
